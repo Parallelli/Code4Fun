00984bc692e18d778357816100465540b8bc40c4
Li Xiong <xiongli.cs@gmail.com>
2014-11-15 21:13:49 -0800

    Update download_pdfs.py

--- download_pdfs.py
+++ download_pdfs.py
@@ -1,8 +1,8 @@
-
+#download all the slides (.pdf) from Greg Mori's site
 import urllib2
 myset = [1,2.5,3,4,5,6,7,'8a','8b',9,11,13,'13b']
 for id in myset:
-    outfile = open('C:\\scratch\\lxa27\\slides\\' + str(id) + '.pdf','wb')
+    outfile = open('C:\\scratch\\lxa27\\slides\\' + str(id) + '.pdf','wb') #wb 
     response = urllib2.urlopen('http://www2.cs.sfu.ca/~mori/courses/cmpt726/fall14/slides/chapter' + str(id)  + '_slides.pdf')
     slides = response.read()
     outfile.write(slides)
